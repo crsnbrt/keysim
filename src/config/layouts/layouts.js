249f9ff9a054bd6fead7dbeb3782c8e8834d13ca
Guillaume Gérard <guillaume.gerard88@gmail.com>
2020-09-28 10:00:38 +0200

    refactor: fix ortho40 to common layout and add ortho50 keyboard

--- src/config/layouts/layouts.js
+++ src/config/layouts/layouts.js
@@ -7,6 +7,7 @@ import layout_65 from "./layout_65_default.json";
 import layout_80 from "./layout_80_default.json";
 import layout_numpad from "./layout_numpad.json";
 import layout_40_ortho from "./layout_40_ortho.json";
+import layout_50_ortho from "./layout_50_ortho.json";
 import layout_40_leftnum from "./layout_40_leftnum.json";
 import layout_95 from "./layout_95_default.json";
 import layout_100 from "./layout_100_default.json";
@@ -22,7 +23,8 @@ const LAYOUTS = {
   "60wkl": layout_60_wkl,
   "60hhkb": layout_60_hhkb,
   numpad: layout_numpad,
-  ortho: layout_40_ortho,
+  "40ortho": layout_40_ortho,
+  "50ortho": layout_50_ortho,
   leftnum: layout_40_leftnum,
 };
 
